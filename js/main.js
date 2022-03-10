66aa7f22f315b6105bb4d74d1127bdff545c089d
Busna27 <lucabusnelli2001@gmail.com>
2022-03-10 14:52:55 +0100

    iniz. array object carousel

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+Riprendiamo l'esercizio carosello e rifacciamolo, questa volta usando gli oggetti, prendendo come riferimento il codice scritto oggi insieme a lezione, che troverete direttamente nella mia repository di github a questo link: [link github]
+
+Modifichiamo il codice dell'esercizio per renderlo funzionante con un array di oggetti al posto dei tre array separati, con una sola regola: non è possibile modificare l'HTML ma solamente JS e CSS.
+Ricordiamo sempre l'importanza dell'integrità del dato.
+
+Bonus 1:
+Sperimentiamo attraverso l'uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:
+al click di un bottone o già dall'inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
+
+Bonus 2:
+E se volessi un bottone per invertire la "direzione" del carosello?
+ */
+
+
