0dc3dca14b00c32b2697cb51809bfa44cf227089
Busna27 <lucabusnelli2001@gmail.com>
2022-03-10 16:13:02 +0100

    do a littlev bit of carousel TODO: fix miniature problem (miniatureContainer is null)

--- js/main.js
+++ js/main.js
@@ -13,3 +13,85 @@ E se volessi un bottone per invertire la "direzione" del carosello?
  */
 
 
+const imageArray = [
+    {
+        src : "img/img-1.jpg",
+        name : "Immagine isola"
+    },
+    {
+        src : "img/img-2.jpg",
+        name : "Immagine deserto"
+    },
+    {
+        src : "img/img-3.jpg",
+        name : "Immagine prateria"
+    },
+    {
+        src : "img/img-4.jpg",
+        name : "Immagine montagne"
+    },
+    {
+        src : "img/img-5.jpg",
+        name : "Immagine lago"
+    }
+]
+
+const imageContainer = document.querySelector('.my-carousel-images');
+const thumbnailContainer = document.querySelector('.my-thumbnails');
+thumbnailContainer.innerHTML =+ `
+<div class="row my-miniature-container"></div>
+`
+const miniatureContainer = document.querySelector('.my-miniature-container');
+const prevBtn = document.querySelector('.my-previous');
+const nextBtn = document.querySelector('.my-next');
+
+
+
+for (let i = 0; i < imageArray.length; i++) {
+    imageContainer.innerHTML += `<img class="my-carousel-item" src="${imageArray[i].src}" alt="${imageArray[i].name}">`
+
+    miniatureContainer.innerHTML += `
+    <div class="col my-min">
+        <img class="my-miniature-item" src="${imageArray[i].src}" alt="${imageArray[i].name}>
+    </div>
+    `
+}
+
+const carouselElements = document.getElementsByClassName('my-carousel-item');
+const miniatureElements = document.getElementsByClassName('my-miniature-item');
+
+console.log(carouselElements);
+
+carouselElements[0].classList.add('my-active');
+miniatureElements[0].classList.add('my-opacity-min');
+let activeElement = 0;
+
+prevBtn.addEventListener('click', function() {
+    // Remove active from the current items
+    carouselElements[activeElement].classList.remove('my-active');
+
+    activeElement--;
+
+    // Loop if condition that return to end
+    if(activeElement < 0) {
+        activeElement = carouselElements.length - 1;
+    }
+
+    // Add active to the previous items
+    carouselElements[activeElement].classList.add('my-active');
+});
+
+nextBtn.addEventListener('click', function() {
+    // Remove active from the current items
+    carouselElements[activeElement].classList.remove('my-active');
+
+    activeElement++;
+
+    // Loop if condition that return to start
+    if(activeElement > (carouselElements.length - 1)) {
+        activeElement = 0;
+    }
+
+    // Add active to the next items
+    carouselElements[activeElement].classList.add('my-active');
+});
